48541c146d4d06af3ff43dd76e590d24ab0e02f7
jerossh <jerosshen@gmail.com>
2016-09-18 23:06:56 +0800

    go for first react-app

---
+++ js/app.jsx
@@ -0,0 +1,18 @@
+/*jshint quotmark:false */
+/*jshint white:false */
+/*jshint trailing:false */
+/*jshint newcap:false */
+/*global React, Router*/
+var app = app || {};
+
+app.ALL_TODOS = 'all';
+app.ACTIVE_TODOS = 'active';
+app.COMPLETED_TODOS = 'completed';
+var TodoFooter = app.TodoFooter;
+var TodoItem = app.TOdoItem;
+
+var ENTER_KEY = 13;
+
+var TodoApp = React.createClass({
+  
+})
---
+++ readme.md
@@ -0,0 +1,11 @@
+# React TodoMVC Example
+
+need http-server module
+
+```
+npm install http-server -g
+
+cd tolist
+
+http-server
+```
