c45400918bd36e44eaf1c7d4aa61a5bc17903d71
Jon_shen <jerosshen@gmail.com>
2016-09-22 15:07:20 +0800

    fix the bug

--- js/app.jsx
+++ js/app.jsx
@@ -12,7 +12,7 @@ var app = app || {};
   app.ACTIVE_TODOS = 'active';
   app.COMPLETED_TODOS = 'completed';
   var TodoFooter = app.TodoFooter;
-  var TodoItem = app.ToDoItem;
+  var TodoItem = app.TodoItem;
 
   var ENTER_KEY = 13;
 
@@ -30,7 +30,7 @@ var app = app || {};
    // 加载完
     componentDidMount: function () {
       var setState = this.setState;
-      var router = router({
+      var router = Router({
         '/': setState.bind(this, {nowShowing: app.ALL_TODOS}),
         '/active': setState.bind(this, {nowShowing: app.ACTIVE_TODOS}),
         '/completed': setState.bind(this, {nowShowing: app.COMPLETED_TODOS}),
@@ -43,10 +43,11 @@ var app = app || {};
       this.setState({newTodo: event.target.value});
     },
 
-    handleNewTodoKeyDown: function() {
+    handleNewTodoKeyDown: function(event) {
       if(event.keyCode !== ENTER_KEY) {
         return;
       }
+
       event.preventDefault();
 
       var val = this.state.newTodo.trim();
@@ -88,97 +89,97 @@ var app = app || {};
     },
 
     render: function () {
-      var footer;
-      var main;
-      var todos = this.props.model.todos;
+			var footer;
+			var main;
+			var todos = this.props.model.todos;
 
-      var shownTodos = todos.filter(function (todo) {
-        switch (this.state.nowShowing) {
-        case app.ACTIVE_TODOS:
-          return !todo.completed;
-        case app.COMPLETED_TODOS:
-          return todo.completed;
-        default:
-        return true;
-        }
-      }, this);  // 这个 this 是干嘛的
+			var shownTodos = todos.filter(function (todo) {
+				switch (this.state.nowShowing) {
+				case app.ACTIVE_TODOS:
+					return !todo.completed;
+				case app.COMPLETED_TODOS:
+					return todo.completed;
+				default:
+					return true;
+				}
+			}, this);
 
-      var todoItems = shownTodos.map(function (todo) {
-        return (    // 有些需要绑定而有些不用绑定
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            onToggle={this.toggle.bind(this, todo)}
-            onDestroy={this.destroy.bind(this, todo)}
-            onEdit={this.edit.bind(this, todo)}
-            editing={this.state.editing === todo.id}
-            onSave={this.save.bind(this, todo)}
-            onCancel={this.cancel}
-          />
-        );
-      }, this);
+			var todoItems = shownTodos.map(function (todo) {
+				return (
+					<TodoItem
+						key={todo.id}
+						todo={todo}
+						onToggle={this.toggle.bind(this, todo)}
+						onDestroy={this.destroy.bind(this, todo)}
+						onEdit={this.edit.bind(this, todo)}
+						editing={this.state.editing === todo.id}
+						onSave={this.save.bind(this, todo)}
+						onCancel={this.cancel}
+					/>
+				);
+			}, this);
 
-      var activeTodoCount = todos.reduce(function (accum, todo) {
-        return todo.completed ? accum : accum + 1;
-      }, 0);
+			var activeTodoCount = todos.reduce(function (accum, todo) {
+				return todo.completed ? accum : accum + 1;
+			}, 0);
 
-      var completedCount = todos.length - activeTodoCount;
+			var completedCount = todos.length - activeTodoCount;
 
-      if (activeTodoCount || completedCount) {
-        footer =
-          <TodoFooter
-            count={activeTodoCount}
-            completedCount={completedCount}
-            nowShowing={this.state.nowShowing}
-            onClearCompleted={this.clearCompleted}
-          />;
-      }
+			if (activeTodoCount || completedCount) {
+				footer =
+					<TodoFooter
+						count={activeTodoCount}
+						completedCount={completedCount}
+						nowShowing={this.state.nowShowing}
+						onClearCompleted={this.clearCompleted}
+					/>;
+			}
 
-      if (todos.length) {
-        main = (
-          <section className='main'>
-            <input
-              className='toggle-all'
-              type="checkbox"
-              onChange={this.toggleAll}
-              checked={activeTodoCount === 0}
-            />
-            <ul className="todo-list">
-              {todoItems}
-            </ul>
-          </section>
-        );
-      }
+			if (todos.length) {
+				main = (
+					<section className="main">
+						<input
+							className="toggle-all"
+							type="checkbox"
+							onChange={this.toggleAll}
+							checked={activeTodoCount === 0}
+						/>
+						<ul className="todo-list">
+							{todoItems}
+						</ul>
+					</section>
+				);
+			}
 
-      return (
-        <div>
-          <header className='header'>
-            <h1>todos</h1>
-            <input
-              className='new-todo'
-              placeholder='what needs to be done'
-              value={this.state.newTodo}
-              onkeyDown={this.handleNewTodoKeyDown}
-              onChange={this.handleChnge}
-              autofocus={true}
-            />
-          </header>
-          {main}
-          {footer}
-        </div>
-      );
-    }
-  });
+			return (
+				<div>
+					<header className="header">
+						<h1>todos</h1>
+						<input
+							className="new-todo"
+							placeholder="What needs to be done?"
+							value={this.state.newTodo}
+							onKeyDown={this.handleNewTodoKeyDown}
+							onChange={this.handleChange}
+							autoFocus={true}
+						/>
+					</header>
+					{main}
+					{footer}
+				</div>
+			);
+		}
+	});
 
-  var model = new app.TodoModel('react-todos');
+	var model = new app.TodoModel('react-todos');
 
-  function render() {
-    React.render(
-      <TodoApp model={model}/>,
-      document.getElementsByClassName('todoapp')[0]
-    );
-  }
+	function render() {
+		React.render(
+			<TodoApp model={model}/>,
+			document.getElementsByClassName('todoapp')[0]
+		);
+	}
 
-  model.subscribe(render);
-  render();
+	model.subscribe(render);
+	render();
 })();
